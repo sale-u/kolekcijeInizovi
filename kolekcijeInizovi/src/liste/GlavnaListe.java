80e1bdbc494fb895614f44ba6015da38a0121281
Sale <Sale@MAJAPC>
2021-07-22 21:47:54 +0200

    liste i osnovne metode lista

--- kolekcijeInizovi/src/liste/GlavnaListe.java
+++ kolekcijeInizovi/src/liste/GlavnaListe.java
@@ -1,6 +1,56 @@
 package liste;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GlavnaListe {
-	// pozdrav
+
+	public static void main(String[] args) {
+
+		// instancirali smo listu sa generickim tipom
+		List<String> sveIsvasta = new ArrayList<String>();
+		
+		sveIsvasta.add("32");
+		sveIsvasta.add("Pera");
+		sveIsvasta.add("Sta hocu...");
+		sveIsvasta.add("...");
+		sveIsvasta.add("32");
+		
+		String izListe = sveIsvasta.get(1);
+		System.out.println(izListe + "\n=====================");
+		
+		for (int i = 0; i < sveIsvasta.size(); i++) {
+			System.out.println(sveIsvasta.get(i));
+		}
+		
+		// izbacio sam iz liste string "..." pod indeksom = 3
+		sveIsvasta.remove(3);
+		System.out.println("Nakon izbacivanja: ");
+		for (int i = 0; i < sveIsvasta.size(); i++) {
+			System.out.println(sveIsvasta.get(i));
+		}
+		
+		String treci = sveIsvasta.get(3);
+		System.out.println("Treci je sad " + treci);
+		
+		// ocisti celu listu
+		sveIsvasta.clear();
+		System.out.println("Nakon ciscenja: ");
+		for (int i = 0; i < sveIsvasta.size(); i++) {
+			System.out.println(sveIsvasta.get(i));
+		}
+		
+		sveIsvasta.add("Nenad");
+		sveIsvasta.add("Ogi");
+		
+		System.out.println("Nakon ponovnog dodavanja: ");
+		// enhanced for loop - for each
+		for (String s : sveIsvasta) {
+			System.out.println(s);
+		}
+		
+		System.out.println("\n" + sveIsvasta);
+
+	}
 
 }
